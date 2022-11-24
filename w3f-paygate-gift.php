47634b472fd6a091e4b586d7ba6bdfb72c1b8bba
Hermann EHO <hermannleboss@gmail.com>
2022-11-24 11:38:03 +0000

    feat: crud structure for api key management

--- w3f-paygate-gift.php
+++ w3f-paygate-gift.php
@@ -14,7 +14,7 @@ add_action( 'admin_menu', 'w3f_options_page' );
 function w3f_options_page() {
 	add_menu_page(
 		'W3F Gift',
-		'W3F Options',
+		'W3F Gift',
 		'manage_options',
 		'w3f',
 		'w3f_options_page_html',
@@ -24,20 +24,40 @@ function w3f_options_page() {
 }
 
 function w3f_options_page_html() {
+	$w3f_api_key = '';
 	?>
     <div class="wrap">
         <h1><?php echo esc_html( get_admin_page_title() ); ?></h1>
-        <form action="" method="post">
-			<?php
+        <form action="<?php menu_page_url( 'w3f' ) ?>" method="post">
+            <label>Entrez la valeur de l'API KEY
+                <input type="text" value="<?php echo( $w3f_api_key ) ?>">
+            </label>
+	        <?php
 			// output security fields for the registered setting "w3f_options"
 			settings_fields( 'w3f_options' );
 			// output setting sections and their fields
 			// (sections are registered for "w3f", each field is registered to a specific section)
 			do_settings_sections( 'w3f' );
 			// output save settings button
-			submit_button( __( 'Save Settings', 'textdomain' ) );
+			submit_button( __( 'Enregister la clé Paygate', 'textdomain' ) );
 			?>
         </form>
     </div>
 	<?php
+}
+
+function w3f_register_api_key() {
+	add_option( "w3f_paygate_api_key", '', '', true );
+}
+
+function w3f_remove_api_key() {
+	delete_option( "w3f_paygate_api_key" );
+}
+
+function w3f_get_api_key() {
+	get_option( "w3f_paygate_api_key", false );
+}
+
+function w3f_update_api_key() {
+	update_option( "w3f_paygate_api_key", "new Value" );
 }
